5eeee7f53e8b333330b6e41c5368f94cac39ece7
root <root@rocinante.lsi.lan>
2019-12-07 09:00:29 -0800

    merge-users-and-groups.py seems to work.

--- merge-users-and-groups.py
+++ merge-users-and-groups.py
@@ -35,76 +35,6 @@ mypasswd = mydb['passwd']
 mygroup = mydb['group']
 myshadow = mydb['shadow']
 
-def is_system_id(id):
-    return (id < 1000) or (id > 60000)
-
-# check for system users in imported db not in local db and warn about
-# them
-
-missing_system_users = []
-for otheruser in otherpasswd.values():
-    if is_system_id(otheruser['pw_uid']) and (not otheruser['pw_name'] in mypasswd):
-        missing_system_users.append(otheruser['pw_name'])
-
-if missing_system_users:
-    print(
-        "\nSystem users on other system not on local system:\n ",
-        ' '.join(missing_system_users),
-        "\n",
-        file=sys.stderr)
-
-# check for system groups in imported db not in local db and warn about
-# them
-
-missing_system_groups = []
-for othergroupvalue in othergroup.values():
-    if is_system_id(othergroupvalue['gr_gid']) and (not othergroupvalue['gr_name'] in mygroup):
-        missing_system_groups.append(othergroupvalue['gr_name'])
-
-if missing_system_groups:
-    print(
-        "\nSystem groups on other system not on local system:\n ",
-        ' '.join(missing_system_groups),
-        "\n",
-        file=sys.stderr)
-
-
-def id_in_use(id_key, id_key_value, dict):
-    """
-    For all tables the primary key is the name and the numeric ID is a unique secondary key.
-    Return true if an ID is already in use.
-    """
-    for value in dict.values():
-        if value[id_key] == id_key_value:
-            return True
-    return False
-
-def get_entry_by_id(id_key, id_key_value, dict):
-    """return the dict entry with matching key value"""
-    for value in dict.values():
-        if value[id_key] == id_key_value:
-            return value
-    pprint(dict)
-    raise KeyError(id_key + "[" + id_key_value + "] not found")
-
-def merge_other_into_my(id_key, name_key, other, my, id_collisions):
-    id_collisions = []
-    for value in other.values():
-        otherid = value[id_key]
-        if not is_system_id(otherid):
-            othername = value[name_key]
-            if not othername in my:
-                if id_in_use(id_key, otherid, my):
-                    id_collisions.append(otherid)
-                    print(" ID collision", otherid, "between",
-                        othername, "there and",
-                        get_entry_by_id(id_key, otherid, my)[name_key],
-                        "here\n")
-                else:
-                    my[otherid] = value
-                #elif my[othername] != value:
-                #   print("", othername, "differs\n")
-
 # change group id number in passwd table to its name to avoid needing
 # to remap it on collision later
 
@@ -121,31 +51,127 @@ def passwd_gid_to_group_name(passwd, groups):
 passwd_gid_to_group_name(otherpasswd, othergroup)
 passwd_gid_to_group_name(mypasswd, mygroup)
 
+def is_system_id(id):
+    return (id < 1000) or (id > 60000)
+
+def id_in_use(id_key, id_key_value, dict):
+    """
+    Return true if an ID is already in use.
+    """
+    for value in dict.values():
+        if value[id_key] == id_key_value:
+            return True
+    return False
+
+# check for system users in imported db not in local db and warn about
+# them
+
+missing_system_users = []
+for otheruser in otherpasswd.values():
+    if is_system_id(otheruser['pw_uid']) and not id_in_use('pw_name', otheruser['pw_name'], mypasswd):
+        missing_system_users.append(otheruser['pw_name'])
+        
+if missing_system_users:
+    print(
+        "System users on other system not on local system:\n ",
+        ' '.join(missing_system_users),
+        #"\n",
+        file=sys.stderr)
+
+# check for system groups in imported db not in local db and warn about
+# them
+
+missing_system_groups = []
+for othergroupvalue in othergroup.values():
+    if is_system_id(othergroupvalue['gr_gid']) and not id_in_use('gr_name', othergroupvalue['gr_name'], mygroup):
+        missing_system_groups.append(othergroupvalue['gr_name'])
+
+if missing_system_groups:
+    print(
+        "System groups on other system not on local system:\n ",
+        ' '.join(missing_system_groups),
+        #"\n",
+        file=sys.stderr)
+
+def get_entry_by_id(id_key, id_key_value, dict):
+    """return the dict entry with matching key value"""
+    for value in dict.values():
+        if value[id_key] == id_key_value:
+            return value
+    #pprint(dict)
+    raise KeyError(id_key + " of " + id_key_value + " not found")
+
+def has_entry_by_id(id_key, id_key_value, dict):
+    """return true if the dict entry has matching key value"""
+    for value in dict.values():
+        if value[id_key] == id_key_value:
+            return True
+    return False
+
+def merge_other_into_my(id_key, name_key, other, my, id_collisions):
+    for value in other.values():
+        otherid = value[id_key]
+        othername = value[name_key]
+        if not has_entry_by_id(name_key, othername, my):
+            if id_in_use(id_key, otherid, my):
+                id_collisions.append(otherid)
+                print(" ID collision", otherid, "between",
+                    othername, "there and",
+                    get_entry_by_id(id_key, otherid, my)[name_key],
+                    "here")
+            else:
+                my[otherid] = value
+            #elif my[othername] != value:
+            #   print("", othername, "differs\n")
+
 # here we do groups first in case we need to fix up the pw_gid on a gid
 # collision
 
 # merge new groups from otherdb into mydb
+
+print("Merging new groups...")
 gid_collisions = []
 merge_other_into_my('gr_gid', 'gr_name', othergroup, mygroup, gid_collisions)
 
 # merge new users from otherdb into mydb
+
+print("Merging new users...")
 uid_collisions = []
 merge_other_into_my('pw_uid', 'pw_name', otherpasswd, mypasswd, uid_collisions)
 
 # now insert new users and groups that had collisions, choosing "holes"
-# above 1000
+# above 1000 for non-system users and 500 for system users
 
+def find_free_id(oldid, destination_dict):
+    '''
+    find id not used as key in destination_dict
+    oldid is used to determine where to start searching
+    '''
+    if is_system_id(oldid):
+        candidate_id = 500
+    else:
+        candidate_id = 1000
+    while True:
+        if not candidate_id in destination_dict:
+            return candidate_id
+        candidate_id = candidate_id + 1
+    
 def insert_new_entries_allocating_ids(ids, id_key, dict_key, source_dict, destination_dict):
     for id in ids:
         source_entry = get_entry_by_id(id_key, id, source_dict)
-        new_entry = copy.deep_copy(source_entry)
-        new_id = find_free_id(id_key, destination_dict)
+        new_entry = copy.deepcopy(source_entry)
+        new_id = find_free_id(id, destination_dict)
         new_entry[id_key] = new_id
         destination_dict[new_id] = new_entry
+        print(" inserted ", new_entry[dict_key])
 
+print("Merging colliding users...")
+pprint(uid_collisions)
 insert_new_entries_allocating_ids(ids=uid_collisions, id_key='pw_uid', dict_key='pw_name',
     source_dict=otherpasswd, destination_dict=mypasswd)
 
+print("Merging colliding groups...")
+pprint(gid_collisions)
 insert_new_entries_allocating_ids(ids=gid_collisions, id_key='gr_gid', dict_key='gr_name',
     source_dict=othergroup, destination_dict=mygroup)
 
@@ -166,15 +192,13 @@ for othershadowvalue in othershadow.values():
             # here we don't have to worry about uid collision
             myshadow[othershadowname] = othershadowvalue
         elif myshadow[othershadowname] != othershadowvalue:
-            print("Shadow", othershadowname, "differs\n")
+            print("Shadow", othershadowname, "differs")
 
 # convert the primary group names back to gids in mypasswd
 
 def group_name_to_gid(groups, name):
-    for group in groups.values():
-        if name  == group["gr_name"]:
-            return group["gr_gid"]
-    raise KeyError("primary group " + name + " in password file not in group file")
+    entry = get_entry_by_id("gr_name", name, groups)
+    return entry["gr_gid"]
 
 for user in mypasswd.values():
     user["pw_gid"] = group_name_to_gid(mygroup, user["pw_gid"])
